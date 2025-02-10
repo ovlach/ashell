aeb6b8fb17ba88587f481a7421426ac5d5af9b72
Ondrej Vlach <ondrej@vlach.xyz>
2025-02-10 22:50:45 +0100

    update flake

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A ready to go Wayland status bar for Hyprland";
 
   inputs = {
-    crane.url = "github:ipetkov/crane";
+    crane.url = "github:ipetkov/crane?branch=v0.20.1";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     rust-overlay = {
